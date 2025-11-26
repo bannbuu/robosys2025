c71438cf58f133f276122a6d1bf27ea315d0ab4b
Yoshiaki Naruse <zhengyit364@gmail.com>
2025-11-26 17:31:11 +0900

    Test both plus and food commands

--- test.bash
+++ test.bash
@@ -7,20 +7,48 @@ ng () {
 
 res=0
 
-### NORMAL INPUT ###
+### plus コマンドのテスト ###
+
+# NORMAL INPUT
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+[ "$?" = 0 ] || ng "$LINENO"          # 正常終了しているか
+[ "${out}" = 15 ] || ng "$LINENO"     # 1+2+3+4+5 = 15 か
 
-### STRANGE INPUT 1: 計算できない値 ###
-out=$(echo あ | ./plus)     # ひらがなを入れてわざとエラー
-[ "$?" = 1 ] || ng "$LINENO"    # 終了ステータスが 1 か？
-[ "${out}" = "" ] || ng "$LINENO"   # 何も出力していないか？
+# STRANGE INPUT 1: 計算できない値
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"          # エラー終了しているか
+[ "${out}" = "" ] || ng "$LINENO"     # 標準出力には何も出ていないか
 
-### STRANGE INPUT 2: 何も入力しない ###
+# STRANGE INPUT 2: 何も入力しない
 out=$(echo | ./plus)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK   # ここまで全部通ったら OK と表示
-exit $res                     # テスト結果を終了ステータスで返す
+
+### food コマンドのテスト ###
+
+# NORMAL INPUT: 肉 / 焼く / 牛
+out=$(./food 肉 焼く 牛)
+[ "$?" = 0 ] || ng "$LINENO"          # 正常終了しているか
+[ -n "${out}" ] || ng "$LINENO"       # 1行以上出力されているか（ステーキ/焼肉など）
+
+# HELP の確認
+out=$(./food --help)
+[ "$?" = 0 ] || ng "$LINENO"          # ヘルプは正常終了であるべき
+# ヘルプの内容まではここではチェックしない
+
+# 未対応の大ジャンル
+out=$(./food 野菜 焼く 牛)
+[ "$?" = 2 ] || ng "$LINENO"          # 想定どおり終了ステータス2か
+[ "${out}" = "" ] || ng "$LINENO"     # 標準出力は空（エラーはstderr）
+
+# 組み合わせが見つからない場合
+out=$(./food 肉 揚げる 羊)
+[ "$?" = 4 ] || ng "$LINENO"          # 想定どおり終了ステータス4か
+[ "${out}" = "" ] || ng "$LINENO"     # 標準出力は空
+
+### すべてのテストが通ったかどうか ###
+
+[ "${res}" = 0 ] && echo OK
+exit $res
 
