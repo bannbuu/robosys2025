3926afa5ad6aeca14f4f707e39ab6f9bab56b81e
Yoshiaki Naruse <zhengyit364@gmail.com>
2025-11-28 22:51:11 +0900

    test10

--- test.bash
+++ test.bash
@@ -45,6 +45,10 @@ out=$(./konndate 魚 蒸す 貝)
 [ "$?" = 0 ] || ng "$LINENO"
 [ "$out" = "アサリの酒蒸し" ] || ng "$LINENO"
 
+out=$(./konndate 麺 汁あり 中華)
+[ "$?" = 0 ] || ng "$LINENO"
+[ "$out" = $'ラーメン\n担々麺' ] || ng "$LINENO"
+
 [ "${res}" = 0 ] && echo OK
 exit $res
 
